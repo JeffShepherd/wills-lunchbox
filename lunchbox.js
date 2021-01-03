763c9aefa70104dcf17c000874de721c9ff03f89
Jeff S <jeff.shep777@gmail.com>
2021-01-03 10:05:49 -0700

    Finish challenege

--- lunchbox.js
+++ lunchbox.js
@@ -1,5 +1,25 @@
 class LunchBox {
-
+  constructor(lunchboxObject) {
+    this.owner = lunchboxObject.owner;
+    this.material = lunchboxObject.madeOf;
+    this.shape = lunchboxObject.shape;
+    this.color = lunchboxObject.color;
+    this.handle = true;
+    this.snacks = [];
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].healthy === true) {
+        healthySnacks.push(this.snacks[i])
+      }
+    }
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
--- snack.js
+++ snack.js
@@ -1,5 +1,18 @@
 class Snack {
-
+  constructor(type) {
+    this.type = type;
+    this.deliciousLevel = 'extra';
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    this.healthy = type.includes('Fruit')
+  }
+  getEaten() {
+    this.amount -= 10;
+    if(this.amount < 21) {
+      this.cuttingItClose = true;
+    }
+  }
 }
 
 module.exports = Snack;
